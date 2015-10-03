ca7c37fa4e03d6eaa94d0fb3949c45a2fc665d45
Sulice <niels.robin.aubertin@gmail.com>
2015-10-03 16:10:17 +0200

    added missing files from last commit

---
+++ JS/landing/main.js
@@ -0,0 +1,47 @@
+
+function start() {
+	$("#landing > .login-form").on("submit", function(e) {
+		e.preventDefault();	
+		action = $(this).children(".action").val();
+		mail = $(this).children(".mail").val();
+		password = $(this).children(".password").val();
+		action = $(this).attr('data-action');
+		$.ajax({
+			url: "Ajax/connect.php",
+			type: "POST",
+			data: {"action":action, "mail":mail, "password":password},
+			success: function(data) {
+					console.log(data);
+					window.location.reload(true); 
+				},
+			error: function() {
+					console.log("fail");
+				}
+		})
+	});
+}
+
+function stop() {
+	$("#landing > .login-form").off();
+}
+
+function switchForm() {
+	if($('#landing .login-form').attr('data-action') == "login") {
+		landing.switchToSignup();
+	} else {
+		landing.switchToLogin();
+	}
+}
+
+function switchToLogin() {
+	$('#landing .signin').html('S\'inscrire');
+	$('#landing .center-form .title-form').html('Se connecter');
+	$('#landing .background').css('background-image','url("Assets/Backgrounds/pre5.jpg")');
+	$('#landing .login-form').attr('data-action','login');
+}
+function switchToSignup() {
+	$('#landing .signin').html('Se connecter');
+	$('#landing .center-form .title-form').html('S\'inscrire');
+	$('#landing .background').css('background-image','url("Assets/Backgrounds/pre2.jpg")');
+	$('#landing .login-form').attr('data-action','signup');
+}
