7459ca48f0b5c6d862da7727fd6f5bb3f9efe520
Niels <niels.robin.aubertin@gmail.com>
2015-11-16 18:15:30 +0100

    reviewed css generation from sass

--- Build/build-css.sh
+++ Build/build-css.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sass SCSS/style.scss:style.css -t compressed
+rnd=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+find SCSS -maxdepth 1 -name "*.scss" > "$rnd"
+sed 's/.*/@import "&";/g' < "$rnd" > style.scss
+sass style.scss:style.css -t compressed
+rm "$rnd" style.scss
 chmod 775 *.css *.map
