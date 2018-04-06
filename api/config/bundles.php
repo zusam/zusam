ca5835345cf9fae2ca1d80bbd2c26f45153cae41
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-04-06 19:59:30 +0200

    Update symfony packages

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
