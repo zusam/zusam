e9afdc99c4d4a43c77bdfe14b1a00ba6cad9cb8d
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-04-06 22:17:05 +0200

    Add placeholder hyperapp app built with babel+parcel

---
+++ app/src/app.js
@@ -0,0 +1,20 @@
+import { h, app } from "hyperapp"
+
+const state = {
+  count: 0
+}
+
+const actions = {
+  down: value => state => ({ count: state.count - value }),
+  up: value => state => ({ count: state.count + value })
+}
+
+const view = (state, actions) => (
+  <div>
+    <h1>{state.count}</h1>
+    <button onclick={() => actions.down(1)}>-</button>
+    <button onclick={() => actions.up(1)}>+</button>
+  </div>
+)
+
+app(state, actions, view, document.body)
