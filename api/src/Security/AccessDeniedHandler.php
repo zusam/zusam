7d3b69905415dc274790e94b3031269090012a0c
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-04-26 15:04:12 +0200

    Add EntryPointService and AccessDeniedHandler

---
+++ api/src/Security/AccessDeniedHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        return new Response(
+            '{"message":"Access denied"}',
+            Response::HTTP_FORBIDDEN,
+            ["Content-type" => "application/json"]
+        );
+    }
+}
---
+++ api/src/Security/EntryPointService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+class EntryPointService implements AuthenticationEntryPointInterface {
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return new Response(
+            '{"message":"You must use an api key"}',
+            Response::HTTP_UNAUTHORIZED,
+            ["Content-type" => "application/json"]
+        );
+    }
+}
