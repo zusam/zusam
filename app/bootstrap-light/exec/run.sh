71d8abe2d322f558ed5f26bbfedcade2ca80cb82
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-04-28 11:13:27 +0200

    Move bootstrap-light submodule into app/

---
+++ app/bootstrap-light/exec/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "%$GROUP ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+adduser -h /btl -D -u "$UID" "$USER"
+grep -q "$GROUP" /etc/group || addgroup -g "$GID" "$GROUP"
+addgroup "$USER" "$GROUP"
+PASSWORD=$(tr -dc "a-zA-Z0-9" < /dev/urandom | fold -w 50 | head -n1)
+echo "$USER:$PASSWORD" | chpasswd 2>/dev/null
+
+su-exec "$UID:$GID" "$@"
