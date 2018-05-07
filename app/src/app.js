d3ebe6afd6143955f2134a1fb5d4ef73f01caf6f
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-05-07 17:55:11 +0200

    Get more data to display the current message

--- app/src/app.js
+++ app/src/app.js
@@ -1,14 +1,15 @@
 import { h, render, Component } from "preact"
 
 class MessageCard extends Component {
-    render(props) {
+    render() {
+        console.log(this.props);
         return (
             <div class="card">
                 <div class="card-header">
-                    { props.author }
+                    <img src={ "/files/" + this.props.author.avatar }/> <span>{ this.props.author.name }</span>
                 </div>
                 <div class="card-body">
-                    <p class="card-text">{ props.data }</p>
+                    <p class="card-text">{ this.props.text }</p>
                 </div>
             </div>
         );
@@ -19,7 +20,7 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            apiKey: "1f1e49e6-0d22-4e3b-9cdd-adf7bd0a53b9",
+            apiKey: "2bccc8f9-cd6f-4a7e-920e-1d6b39ad1f27",
             currentMessage: null,
         }
 
@@ -28,38 +29,47 @@ class App extends Component {
         }
 
         this.http = {
-            getMessage: (id, apiKey) => fetch("/api/messages/"+id, {
-                method: "GET",
-                headers: new Headers({
-                    "Content-type": "application/json",
-                    "X-AUTH-TOKEN": apiKey,
-                })
-            })
+            get: url => {
+                return fetch(url, {
+                    method: "GET",
+                    headers: new Headers({
+                        "Content-type": "application/json",
+                        "X-AUTH-TOKEN": this.state.apiKey,
+                    }),
+                }).then(
+                    res => res.json()
+                ).catch(
+                    err => console.warn(err)
+                );
+            }
         }
     }
 
     syncWithRoute() {
         const segments = this.router.getSegments();
         if (segments[0] === "messages" && segments[1]) {
-            this.http.getMessage(segments[1], this.state.apiKey).then(
-                res => res.json().then(res => {
-                    this.setState({currentMessage: res});
-                })
-            ).catch(e => console.warn(e));
+            this.http.get("/api/" + segments[0] + "/" + segments[1]).then(msg =>
+                this.http.get(msg.author).then(author =>
+                    this.http.get(author.avatar).then(avatar =>
+                        this.setState({
+                            currentMessage: {
+                                author: {
+                                    name: author.login,
+                                    avatar: avatar.id + avatar.extension
+                                },
+                                text: msg.data
+                            }
+                        })
+                    )
+                )
+            );
         }
     }
 
-    render(props, state) {
+    render(_, state) {
         return (
             <div> {
-                state.currentMessage ?
-                    (
-                        <MessageCard 
-                            author={state.currentMessage.author}
-                            data={state.currentMessage.data}
-                        />
-                    )
-                    : ""
+                state.currentMessage ?  (<MessageCard {...state.currentMessage} />) : ""
             } </div>
         );
     }
