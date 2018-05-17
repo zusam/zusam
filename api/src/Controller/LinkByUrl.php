be9b7b4347d0636ace532ed884bf4f053f63eba0
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-05-17 16:28:29 +0200

    Add GET route for LinkByUrl and enable caching

--- api/src/Controller/LinkByUrl.php
+++ api/src/Controller/LinkByUrl.php
@@ -13,17 +13,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class LinkByUrl extends Controller
 {
     /**
-     * @Route("/links/by_url", name="api_links_post_by_url")
+     * @Route("/links/by_url", name="api_links_post_by_url", methods="post")
      */
-    public function index(Request $request, ImageService $imageService): JsonResponse
+    public function getLinkByPost(Request $request, ImageService $imageService): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
         $url = $data["url"] ?? "";
-		if (!$url) {
+        if (!$url) {
             return new JsonResponse(["message" => "Invalid url"], JsonResponse::HTTP_BAD_REQUEST);
-		}
+        }
         $rescan = $data["rescan"] ?? false;
         $onlyData = $data["onlyData"] ?? false;
+        return $this->execute($url, $rescan, $onlyData, $imageService);
+    }
+
+    /**
+     * @Route("/links/by_url", name="api_links_get_by_url", methods="get")
+     */
+    public function getLinkByGet(Request $request, ImageService $imageService): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $url = $request->query->get("url") ?? "";
+        if (!$url) {
+            return new JsonResponse(["message" => "Invalid url"], JsonResponse::HTTP_BAD_REQUEST);
+        }
+        return $this->execute(rawurldecode($url), false, false, $imageService);
+    }
+
+    private function execute($url, $rescan, $onlyData, $imageService): JsonResponse
+    {
         $em = $this->getDoctrine()->getManager();
         $link = $this->getDoctrine()->getRepository(Link::class)->findOneByUrl($url);
         if (empty($link) || $rescan) {
@@ -46,13 +64,21 @@ class LinkByUrl extends Controller
             $em->flush();
         }
         if (!$onlyData) {
-            return new JsonResponse([
+            $data = [
                 "id" => "/api/links/".$link->getId(),
                 "data" => $link->getData(),
                 "url" => $url,
                 "updatedAt" => $link->getUpdatedAt(),
                 "preview" => $link->getPreview() ? $link->getPreview()->getPath() : "",
-            ], JsonResponse::HTTP_OK);
+            ];
+            $response = new JsonResponse($data, JsonResponse::HTTP_OK);
+            $response->setCache(array(
+                "etag"          => md5(json_encode($data)),
+                "max_age"       => 0,
+                "s_maxage"      => 3600,
+                "public"        => true,
+            ));
+            return $response;
         } else {
             return new JsonResponse(json_decode($link->getData(), true), JsonResponse::HTTP_OK);
         }
