b99d56c0e491f70b68cef49cdc9ccb2458ab2abb
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-05-27 11:41:50 +0200

    Increase http module robustness a bit

--- app/src/http.js
+++ app/src/http.js
@@ -1,7 +1,7 @@
-let http = {
+const http = {
 	apiKey: "f759248b-de35-4cc0-ae4b-f37805f72a85",
 	get: url => {
-		return fetch(url, {
+		return url && fetch(url, {
 			method: "GET",
 			headers: new Headers({
 				"Content-type": "application/json",
@@ -14,7 +14,7 @@ let http = {
 		);
 	},
 	post: (url, data) => {
-		return fetch(url, {
+		return url && fetch(url, {
 			method: "POST",
 			body: JSON.stringify(data),
 			headers: new Headers({
@@ -28,10 +28,10 @@ let http = {
 		);
 	},
 	// get the id of an object from an url
-	getId: url => url.split("/").pop().replace(/\?.*$/, "").replace(/\.\w+$/, ""),
+	getId: url => url && url.split("/").pop().replace(/\?.*$/, "").replace(/\.\w+$/, ""),
 	// get the url to a thubmnail (should be used with a file url)
-	thumbnail: (url, width, height) => "/api/images/thumbnail/" + width + "/" + height + "/" + http.getId(url),
+	thumbnail: (url, width, height) => url && "/api/images/thumbnail/" + width + "/" + height + "/" + http.getId(url),
 	// same as http.thumbnail but for a crop
-	crop: (url, width, height) => "/api/images/crop/" + width + "/" + height + "/" + http.getId(url),
+	crop: (url, width, height) => url && "/api/images/crop/" + width + "/" + height + "/" + http.getId(url),
 };
 export default http;
