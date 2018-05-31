5765715e2498da09c23bb03d76efa23d37663326
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-05-31 10:56:22 +0200

    Finish design of message preview

--- app/src/message-preview.component.js
+++ app/src/message-preview.component.js
@@ -1,5 +1,6 @@
 import { h, render, Component } from "preact";
 import http from "./http.js";
+import FaIcon from "./fa-icon.component.js";
 
 export default class MessagePreview extends Component {
     constructor(props) {
@@ -8,10 +9,12 @@ export default class MessagePreview extends Component {
         http.get(this.props.url).then(
             msg => {
                 this.setState({message: msg});
+                http.get(msg.author).then(author => this.setState({author: author}));
                 if (msg.files && msg.files.length > 0) {
                     this.setState({preview: http.crop(msg.files[0], 320, 180)});
                 } else {
                     if (msg.data) {
+                        this.setState({data: JSON.parse(msg.data)});
                         let previewUrl = JSON.parse(msg.data)["text"].match(/https?:\/\/[^\s]+/gi);
                         if (previewUrl) {
                             http.get("/api/links/by_url?url=" + encodeURIComponent(previewUrl[0])).then(r => {
@@ -24,12 +27,32 @@ export default class MessagePreview extends Component {
         );
     }
 
+    getTitle() {
+        if (!this.state.data.title || this.state.data.title.length < 30) {
+            return this.state.data.title && " ";
+        }
+        return this.state.data.title.slice(0, 27) + "...";
+    }
+
     render() {
         return this.state.message && (
-            <a href={ "/messages/" + this.state.message.id }>
-                <div class="card mb-1 ml-1" style="max-width: 400px">
+            <a class="d-block mb-1 ml-1 seamless-link" href={ "/messages/" + this.state.message.id }>
+                <div class="card message-preview shadow-sm">
+                    { this.state.author && this.state.author.avatar && <img class="avatar material-shadow" src={ http.crop(this.state.author.avatar, 80, 80) } /> }
                     { this.state.preview && <img class="card-img-top" src={ http.crop(this.state.preview, 320, 180) } /> }
                     { !this.state.preview && <img class="card-img-top" src="https://zus.am/Assets/placeholder-mini-post.png" /> }
+                    <div class="card-body border-top d-flex justify-content-between">
+                        <span class="left-buffer"></span>
+                        <span class="title">{ this.getTitle() }</span>
+                        <span class="children">
+                            { !!this.state.message.children.length && (
+                                <span>
+                                    { this.state.message.children.length + " " }
+                                    <FaIcon family={"regular"} icon={"comment"} />
+                                </span>
+                            )}
+                        </span>
+                    </div>
                 </div>
             </a>
         );
