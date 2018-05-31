522313e2736fc56bf3b026d9147f2f94b4f8e0cc
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-05-31 15:25:34 +0200

    Allow fullscreen on youtube embed

--- app/src/youtube-embed.js
+++ app/src/youtube-embed.js
@@ -6,7 +6,7 @@ export default class YoutubeEmbed extends Component {
         if (this.state.revealed) {
             const youtubeId = this.props.url.split("/")[3].split("=")[1];
             const embedUrl = "https://www.youtube.com/embed/" + youtubeId + "?autoplay=1&controls=2&wmode=opaque";
-            return <div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src={embedUrl}></iframe></div>;
+            return <div class="embed-responsive embed-responsive-16by9"><iframe allowfullscreen class="embed-responsive-item" src={embedUrl}></iframe></div>;
         }
         return <div class="embed-preview" onClick={() => this.setState({revealed: true})}><div class="youtube"></div><img src={ http.crop(this.props.preview, 320, 180) } /></div>;
     }
