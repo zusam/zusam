1d9d05c884e1fe9b4a579e6b7047cd8ed8f71398
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-06-01 14:29:46 +0200

    Add a router module

---
+++ app/src/router.js
@@ -0,0 +1,30 @@
+import http from "./http.js";
+import store from "./store.js";
+const router = {
+    getSegments: () => window.location.pathname.slice(1).split("/"),
+    navigate: (url, replace = false) => {
+        const [family, id] = url.slice(1).split("/")
+        switch (family) {
+            case "messages":
+            case "groups":
+                if (replace) {
+                    history.replaceState(null, "", url);
+                } else {
+                    history.pushState(null, "", url);
+                }
+                break;
+            default:
+                if (store.currentUser && store.currentUser.groups) {
+                    const url = "/groups/" + http.getId(store.currentUser.groups[0]);
+                    if (replace) {
+                        history.replaceState(null, "", url);
+                    } else {
+                        history.pushState(null, "", url);
+                    }
+                }
+        }
+        window.dispatchEvent(new Event("routerStateChange"));
+    },
+    sync: () => router.navigate(window.location.pathname, true)
+};
+export default router;
