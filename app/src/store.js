72120008f0cd713439c5f6e9aed7b852bd2980d6
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-06-02 16:36:42 +0200

    Allow saving arbitrary data inside the store

--- app/src/store.js
+++ app/src/store.js
@@ -1,11 +1,24 @@
 import http from "./http.js";
 const store = {
     data: {},
+    remove: id => delete(store.data[id]),
+    set: (id, data, cacheDuration = Infinity, persistant = true) => store.data[id] = {
+        data: data,
+        timestamp: Date.now(),
+        cacheDuration: cacheDuration,
+        persistant: persistant,
+    },
     get: (url, cacheDuration = 60*1000, persistant = false) => {
         if (store.data[url] && store.data[url].timestamp + store.data[url].cacheDuration > Date.now()) {
             return new Promise((resolve, _) => resolve(store.data[url].data));
         }
-        return store.update(url, cacheDuration, persistant);
+        // if it's an api resource, refresh it
+        if (/^\/api/.test(url)) {
+            return store.update(url, cacheDuration, persistant);
+        }
+        // if it's too old, remove it and return null
+        store.remove(url);
+        return null;
     },
     update: (url, cacheDuration = 60*1000, persistant = false) => http.get(url).then(res => {
         store.data[url] = {
@@ -16,7 +29,7 @@ const store = {
         };
         return new Promise((resolve, _) => resolve(res));
     }),
-    retrieveData: () => store.data = JSON.parse(window.localStorage.getItem("data")),
-    saveData: () => window.localStorage.setItem("data", JSON.stringify(store.data, e => e.persistant ? e : undefined))
+    retrieveData: () => store.data = JSON.parse(window.localStorage.getItem("data") || "{}") || {},
+    saveData: () => window.localStorage.setItem("data", JSON.stringify(store.data, (_,v) => v.persistant === false ? undefined : v))
 };
 export default store;
