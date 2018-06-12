e2acd9db9e85facd347948ee1021051bd45b67b7
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-06-12 21:34:06 +0200

    Modularize LinkByUrl controller

--- api/src/Controller/LinkByUrl.php
+++ api/src/Controller/LinkByUrl.php
@@ -1,21 +1,32 @@
 <?php
 namespace App\Controller;
 
-use App\Entity\Link;
 use App\Entity\File;
-use App\Service\Url;
+use App\Entity\Link;
 use App\Service\Image as ImageService;
+use App\Service\Url;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LinkByUrl extends Controller
 {
+
+    private $em;
+    private $imageService;
+
+    public function __construct(EntityManagerInterface $em, ImageService $imageService)
+    {
+        $this->em = $em;
+        $this->imageService = $imageService;
+    }
+
     /**
      * @Route("/links/by_url", name="api_links_post_by_url", methods="post")
      */
-    public function getLinkByPost(Request $request, ImageService $imageService): JsonResponse
+    public function getLinkByPost(Request $request): JsonResponse
     {
         $this->denyAccessUnlessGranted("ROLE_USER");
         $data = json_decode($request->getContent(), true);
@@ -25,26 +36,38 @@ class LinkByUrl extends Controller
         }
         $rescan = $data["rescan"] ?? false;
         $onlyData = $data["onlyData"] ?? false;
-        return $this->execute($url, $rescan, $onlyData, $imageService);
+        return $this->execute($url, $rescan, $onlyData);
     }
 
     /**
      * @Route("/links/by_url", name="api_links_get_by_url", methods="get")
      */
-    public function getLinkByGet(Request $request, ImageService $imageService): JsonResponse
+    public function getLinkByGet(Request $request): JsonResponse
     {
         $this->denyAccessUnlessGranted("ROLE_USER");
         $url = $request->query->get("url") ?? "";
         if (!$url) {
             return new JsonResponse(["message" => "Invalid url"], JsonResponse::HTTP_BAD_REQUEST);
         }
-        return $this->execute(rawurldecode($url), false, false, $imageService);
+        return $this->execute(rawurldecode($url));
     }
 
-    private function execute($url, $rescan, $onlyData, $imageService): JsonResponse
+    private function execute($url, $rescan = false, $onlyData = false): JsonResponse
     {
-        $em = $this->getDoctrine()->getManager();
-        $link = $this->getDoctrine()->getRepository(Link::class)->findOneByUrl($url);
+        $data = $this->getLinkData($url, $rescan, $onlyData);
+        $response = new JsonResponse($data, JsonResponse::HTTP_OK);
+        $response->setCache(array(
+            "etag"          => md5(json_encode($data)),
+            "max_age"       => 0,
+            "s_maxage"      => 3600,
+            "public"        => true,
+        ));
+        return $response;
+    }
+
+    public function getLinkData($url, $rescan = false, $onlyData = false): array
+    {
+        $link = $this->em->getRepository(Link::class)->findOneByUrl($url);
         if (empty($link) || $rescan) {
             if (empty($link)) {
                 $link = new Link($url);
@@ -57,31 +80,23 @@ class LinkByUrl extends Controller
                 $preview->setType("image/jpeg");
                 $preview->setExtension(".jpg");
                 $publicDir = realpath($this->getParameter("kernel.project_dir")."/../public/");
-                $imageService->createThumbnail($data["image"], $publicDir."/".$preview->getPath(), 2048, 2048);
+                $this->imageService->createThumbnail($data["image"], $publicDir."/".$preview->getPath(), 2048, 2048);
                 $link->setPreview($preview);
-                $em->persist($preview);
+                $this->em->persist($preview);
             }
-            $em->persist($link);
-            $em->flush();
+            $this->em->persist($link);
+            $this->em->flush();
         }
         if (!$onlyData) {
-            $data = [
+            return [
                 "id" => "/api/links/".$link->getId(),
                 "data" => $link->getData(),
                 "url" => $url,
                 "updatedAt" => $link->getUpdatedAt(),
                 "preview" => $link->getPreview() ? $link->getPreview()->getPath() : "",
             ];
-            $response = new JsonResponse($data, JsonResponse::HTTP_OK);
-            $response->setCache(array(
-                "etag"          => md5(json_encode($data)),
-                "max_age"       => 0,
-                "s_maxage"      => 3600,
-                "public"        => true,
-            ));
-            return $response;
         } else {
-            return new JsonResponse(json_decode($link->getData(), true), JsonResponse::HTTP_OK);
+            return json_decode($link->getData(), true);
         }
     }
 }
