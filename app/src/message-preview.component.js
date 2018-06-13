280490f3107ebf075cd0a3aa90e34b68555aee6c
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-06-13 11:16:37 +0200

    Rework message-preview placeholder styling

--- app/src/message-preview.component.js
+++ app/src/message-preview.component.js
@@ -6,7 +6,10 @@ import FaIcon from "./fa-icon.component.js";
 export default class MessagePreview extends Component {
     constructor(props) {
         super(props);
-        bee.get(this.props.url).then(
+        this.state = {
+            url: props.url,
+        };
+        bee.get(props.url).then(
             msg => {
                 this.setState({message: msg});
                 bee.get(msg.author).then(author => this.setState({author: author}));
@@ -15,16 +18,16 @@ export default class MessagePreview extends Component {
                         hasNews: !!lastVisit && lastVisit.timestamp < msg.lastActivityDate
                     })
                 );
+                this.setState({data: JSON.parse(msg.data)});
                 if (msg.files && msg.files.length > 0) {
                     this.setState({preview: bee.crop(msg.files[0], 320, 180)});
                 } else {
                     if (msg.data) {
-                        this.setState({data: JSON.parse(msg.data)});
                         let previewUrl = JSON.parse(msg.data)["text"].match(/https?:\/\/[^\s]+/gi);
                         if (previewUrl) {
-                            bee.get("/api/links/by_url?url=" + encodeURIComponent(previewUrl[0])).then(r => {
-                                return this.setState({preview: r["preview"]});
-                            });
+                            bee.get("/api/links/by_url?url=" + encodeURIComponent(previewUrl[0])).then(
+                                r => this.setState({preview: r["preview"]})
+                            )
                         }
                     }
                 }
@@ -33,9 +36,6 @@ export default class MessagePreview extends Component {
     }
 
     getTitle() {
-        if (!this.state.data) {
-            return false;
-        }
         if (!this.state.data.title || this.state.data.title.length < 30) {
             return this.state.data.title && " ";
         }
@@ -43,13 +43,34 @@ export default class MessagePreview extends Component {
     }
 
     render() {
-        return this.state.message && (
-            <a class="d-block seamless-link message-preview" href={router.toApp(this.props.url)} onClick={router.onClick}>
+        if (!this.state.message || !this.state.data) {
+            return <a href={this.state.url} class="message-preview-placeholder material-shadow"></a>;
+        }
+
+        return (
+            <a class="d-block seamless-link message-preview" href={router.toApp(this.state.url)} onClick={router.onClick}>
                 <div class="card material-shadow">
-                    { this.state.author && this.state.author.avatar && <img class="avatar material-shadow" src={ bee.crop(this.state.author.avatar, 80, 80) } /> }
-                    { this.state.preview ? 
+                    { this.state.author && this.state.author.avatar && <img class="avatar material-shadow" src={ bee.crop(this.state.author.avatar, 100, 100) } /> }
+                    { this.state.preview ?
                             <img class="card-img-top" src={ bee.crop(this.state.preview, 320, 180) } />
-                            : <div class="card-img-top placeholder"></div>
+                            : (
+                                <svg fill="#ccc" class="card-img-top" viewBox="0 0 320 180">
+                                    <path d="M67,13.4v14.4h-7.5l-0.7-7.5H44.3v51.4l7.3,1.3v5.7H28.2v-5.7l7.3-1.3V20.4H20.9l-0.6,7.5h-7.6V13.4H67z"/>
+                                    <rect x="76" y="14" width="78" height="11"/>
+                                    <rect x="76" y="34" width="78" height="11"/>
+                                    <rect x="76" y="54" width="78" height="11"/>
+                                    <rect x="76" y="74" width="78" height="11"/>
+                                    <rect x="12" y="94" width="141" height="11"/>
+                                    <rect x="12" y="115" width="141" height="11"/>
+                                    <rect x="12" y="135" width="141" height="11"/>
+                                    <rect x="12" y="155" width="141" height="11"/>
+                                    <rect x="168" y="14" width="141" height="11"/>
+                                    <rect x="168" y="34" width="141" height="11"/>
+                                    <rect x="168" y="54" width="141" height="11"/>
+                                    <rect x="168" y="74" width="141" height="11"/>
+                                    <rect x="168" y="94" width="141" height="72"/>
+                                </svg>
+                            )
                     }
                     <div class="card-body border-top d-flex justify-content-between">
                         <span class="left-buffer"></span>
