3e5e3af1dec4d4a1f425d710b17bec6876b22221
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-06-20 12:02:13 +0200

    Add preview to writer

--- app/src/writer.component.js
+++ app/src/writer.component.js
@@ -3,12 +3,14 @@ import lang from "./lang.js";
 import bee from "./bee.js";
 import FaIcon from "./fa-icon.component.js";
 import router from "./router.js";
+import PreviewBlock from "./preview-block.component.js";
 
 export default class Writer extends Component {
 
     constructor(props) {
         super(props);
         this.postMessage = this.postMessage.bind(this);
+        this.getPreview = this.getPreview.bind(this);
     }
 
     postMessage() {
@@ -33,17 +35,36 @@ export default class Writer extends Component {
         });
     }
 
+    getPreview(event) {
+        if (![" ", "Enter", "v"].includes(event.key)) {
+            return;
+        }
+        // waiting for the dom to be updated
+        setTimeout(() => {
+            const text = document.getElementById("text").value;
+            let links = text.match(/(https?:\/\/[^\s]+)/gi);
+            if (links && links[0] != this.state.link) {
+                bee.get("/api/links/by_url?url=" + encodeURIComponent(links[0])).then(r => r && this.setState({
+                    link: links[0],
+                    preview: r,
+                }));
+            }
+        }, 0);
+    }
+
     render() {
         return (
             <div class="writer">
                 { !this.props.parent && <input type="text" id="title" placeholder={lang.fr["title_placeholder"]}></input> }
                 <textarea
+                    onKeyPress={this.getPreview}
                     id="text"
                     placeholder={lang.fr["text_placeholder"]}
                     rows="5"
                     autocomplete="off"
                     autofocus
                 ></textarea>
+                { this.state.preview && <p class="card-text"><PreviewBlock {...this.state.preview} /></p> }
                 <div class="options">
                     <button class="option"><FaIcon family={"regular"} icon={"images"}/></button>
                     <button class="option"><FaIcon family={"solid"} icon={"film"}/></button>
