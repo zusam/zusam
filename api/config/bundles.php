54d82e5d20aa83080e206e3c41429d34520281f4
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-07-03 09:20:23 +0200

    Add swiftmailer

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
