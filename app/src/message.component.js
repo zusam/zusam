6401041429f5d32511b19dbf075485a42ff383ff
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-10-17 18:40:31 +0200

    Update package.json and minor fixes

--- app/src/message.component.js
+++ app/src/message.component.js
@@ -158,7 +158,7 @@ export default class Message extends Component {
                             <div class="message-head p-1 d-none d-md-block">
                                 <img
                                     class="rounded-circle w-3 material-shadow avatar"
-                                    src={ bee.crop(this.props.currentUser.avatar, 100, 100) }
+                                    src={ bee.crop(this.props.currentUser.avatar, 100, 100) || util.defaultAvatar }
                                 />
                             </div>
                         )}
