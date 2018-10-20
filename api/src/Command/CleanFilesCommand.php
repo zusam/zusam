778d20897022611f44827825645b52513ddc2f8c
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-10-20 17:37:49 +0200

    Fix and improve CleanFilesCommand

--- api/src/Command/CleanFilesCommand.php
+++ api/src/Command/CleanFilesCommand.php
@@ -6,20 +6,10 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Doctrine\ORM\EntityManagerInterface;
 
 class CleanFilesCommand extends ContainerAwareCommand
 {
-    private $em;
     private $pdo;
-    private $output;
-    private $input;
-
-    public function __construct(EntityManagerInterface $em)
-    {
-        parent::__construct();
-        $this->em = $em;
-    }
 
     protected function configure()
     {
@@ -32,18 +22,19 @@ class CleanFilesCommand extends ContainerAwareCommand
     {
         $dsn = $this->getContainer()->getParameter("database_url");
         $this->pdo = new \PDO($dsn, null, null);
-        $this->output = $output;
-        $this->input = $input;
-
+        $filesDir = realpath($this->getContainer()->getParameter("dir.files"));
 
         // First we get all files not linked to a user and/or a message
-        $c = $this->pdo->query("SELECT id FROM file WHERE id NOT IN (SELECT file_id FROM messages_files) AND id NOT IN (SELECT avatar_id FROM user);");
+        $c = $this->pdo->query("SELECT id, content_url FROM file WHERE id NOT IN (SELECT file_id FROM messages_files) AND id NOT IN (SELECT avatar_id FROM user WHERE avatar_id NOT NULL);");
         while($i = $c->fetch()) {
             $this->pdo->query("DELETE FROM file WHERE id = '" . $i["id"] . "';");
+            if ($input->getOption("verbose")) {
+                echo "unlink $filesDir/".$i["content_url"]."\n";
+            }
+            unlink($filesDir."/".$i["content_url"]);
         }
 
         // We need to get all files without an entry in the database
-        $filesDir = realpath($this->getContainer()->getParameter("dir.files"));
         if (!is_writeable($filesDir)) {
             throw new \Exception("Target directory ($filesDir) is not writable !");
         }
@@ -52,8 +43,12 @@ class CleanFilesCommand extends ContainerAwareCommand
                 continue;
             }
             $id = pathinfo($file, PATHINFO_FILENAME);
-            $c = $this->pdo->query("SELECT id FROM file WHERE id = '" . $id . "';")->fetch();
-            if (empty($c)) {
+            $c = $this->pdo->query("SELECT id, content_url FROM file WHERE id = '" . $id . "';")->fetch();
+            // remove the file if not in the database or if the content_url doesn't match the filename
+            if (empty($c) || $c["content_url"] != $file) {
+                if ($input->getOption("verbose")) {
+                    echo "unlink $filesDir/$file\n";
+                }
                 unlink($filesDir."/".$file);
             }
         }
