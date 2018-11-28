0e8468308d95bfbf81b0c000ae3689ce350cd76f
Niels Robin-Aubertin <git@niels.fr>
2018-11-28 08:58:42 +0100

    Don't convert videos not linked to a message/user and use PDO

--- api/src/Command/ConvertVideoCommand.php
+++ api/src/Command/ConvertVideoCommand.php
@@ -2,22 +2,21 @@
 
 namespace App\Command;
 
+use App\Entity\File;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\File;
 
 class ConvertVideoCommand extends ContainerAwareCommand
 {
-    private $em;
 	private $ffmpegPath;
+    private $pdo;
 
-    public function __construct(EntityManagerInterface $em, $binaries)
+    public function __construct($binaries)
     {
         parent::__construct();
-        $this->em = $em;
 		$this->ffmpegPath = $binaries["ffmpeg"];
     }
 
@@ -30,25 +29,22 @@ class ConvertVideoCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $dsn = $this->getContainer()->getParameter("database_url");
+        $this->pdo = new \PDO($dsn, null, null);
         $filesDir = realpath($this->getContainer()->getParameter("dir.files"));
-        $rawFiles = $this->em->getRepository(File::class)->findByStatus(File::STATUS_RAW);
-        foreach($rawFiles as $rawFile) {
-            if (substr($rawFile->getType(), 0, 5) === "video") {
-                $outputFile = $rawFile->getId().".mp4";
-				$output->writeln(["Converting ".$rawFile->getContentUrl()]);	
-                exec(
-                    $this->ffmpegPath
-                    ." -y -i ".$filesDir."/".$rawFile->getContentUrl()
-                    ." -c:v libx264 -filter:v scale=-2:720 -crf 22 -threads 1 -preset slower -c:a libmp3lame -y -f mp4 "
-                    .$filesDir."/".$outputFile
-                );
-                $rawFile->setContentUrl($outputFile); 
-                $rawFile->setStatus(File::STATUS_READY); 
-                $rawFile->setType("video/mp4"); 
-                $this->em->persist($rawFile);
-                $this->em->flush();
-                return;
-            }
+        $c = $this->pdo->query("SELECT id, content_url FROM file WHERE (id IN (SELECT file_id FROM messages_files) OR id IN (SELECT avatar_id FROM user WHERE avatar_id NOT NULL)) AND status = '".File::STATUS_RAW."' AND type LIKE 'video%';");
+        while($rawFile = $c->fetch()) {
+            $outputFile = $rawFile["id"].".mp4";
+            $output->writeln(["Converting ".$rawFile["content_url"]]);	
+            exec(
+                $this->ffmpegPath
+                ." -y -i ".$filesDir."/".$rawFile["content_url"]
+                ." -c:v libx264 -filter:v scale=-2:720 -crf 22 -threads 1 -preset slower -c:a libmp3lame -y -f mp4 "
+                .$filesDir."/".$outputFile
+            );
+            $q = $this->pdo->prepare("UPDATE file SET content_url = '".$outputFile."', status = '".File::STATUS_READY."', type = 'video/mp4' WHERE id = '".$rawFile["id"]."';");
+            $q->execute();
+            return;
         }
     }
 }
