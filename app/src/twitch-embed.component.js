8c0a5e4364b692a3309b5570b1384bbe466fd6b6
Niels Robin-Aubertin <git@niels.fr>
2019-01-13 15:40:21 +0100

    Directly embed twitch videos and clips

--- app/src/twitch-embed.component.js
+++ app/src/twitch-embed.component.js
@@ -3,19 +3,16 @@ import bee from "./bee.js";
 
 export default class TwitchEmbed extends Component {
     render() {
-        if (this.state.revealed) {
-            let embedUrl = "";
-            const splits = this.props.url.split("/");
-            switch (splits[2]) {
-                case "www.twitch.tv":
-                    embedUrl = "https://player.twitch.tv/?video=" + splits[4].replace(/\?.*$/,"");
-                    break;
-                case "clips.twitch.tv":
-                    embedUrl = "https://clips.twitch.tv/embed?clip=" + splits[3].replace(/\?.*$/,"");
-                    break;
-            }
-            return <div class="embed-responsive embed-responsive-16by9"><iframe allowfullscreen class="embed-responsive-item" src={embedUrl}></iframe></div>;
+        let embedUrl = "";
+        const splits = this.props.url.split("/");
+        switch (splits[2]) {
+            case "www.twitch.tv":
+                embedUrl = "https://player.twitch.tv/?video=" + splits[4].replace(/\?.*$/,"") + "&autoplay=false";
+                break;
+            case "clips.twitch.tv":
+                embedUrl = "https://clips.twitch.tv/embed?clip=" + splits[3].replace(/\?.*$/,"") + "&autoplay=false";
+                break;
         }
-        return <div class="embed-preview" onClick={() => this.setState({revealed: true})}><div class="twitch"></div><img src={ bee.crop(this.props.preview, 1024, 270) } /></div>;
+        return <div class="embed-responsive embed-responsive-16by9"><iframe allowfullscreen class="embed-responsive-item" src={embedUrl}></iframe></div>;
     }
 }
