1d255e89343c6f09979da20f49643c21cd226729
Niels Robin-Aubertin <git@niels.fr>
2019-01-20 15:33:41 +0100

    Periodically clean cache

--- app/src/cache.js
+++ app/src/cache.js
@@ -14,7 +14,11 @@ const cache = {
         delete(cache.data[id]);
         window.localStorage.removeItem(id);
     },
-    set: (id, data, cacheDuration = Infinity, persistant = true) => {
+    set: (id, data, cacheDuration = null, persistant = true) => {
+        // sometimes clean cache
+        if (Math.random() > 0.9) {
+            cache.cleanCache();
+        }
         const storageBox = {
             data: data,
             timestamp: Date.now(),
@@ -27,27 +31,39 @@ const cache = {
         }
         return Promise.resolve(id);
     },
+    handleCache: id => {
+        if (!id) {
+            return null;
+        }
+        // not persistant data has more priority
+        let data = cache.data[id];
+        if (!data) {
+            data = window.localStorage.getItem(id);
+            data = data ? JSON.parse(data) : null;
+        }
+        if (data) {
+            let c = data.cacheDuration || 1000*60*60*24*365; // 1 year when not specified
+            if (data.timestamp > Date.now() - c) {
+                return data;
+            }
+        }
+        // if it's too old, remove it and return null
+        cache.remove(id);
+        return null;
+    },
     get: (id, nocache = false) => {
         if (!id) {
             return;
         }
         // if nocache is false, try to load from the cache
         if (!nocache) {
-            // not persistant data has more priority
-            let data = cache.data[id];
-            if (!data) {
-                data = window.localStorage.getItem(id);
-                data = data ? JSON.parse(data) : null;
-            }
+            let data = cache.handleCache(id);
             if (data) {
                 if (data.status == "pending") {
                     // a request for the resource was made. Let's wait for it
                     return new Promise(r => cache.register(id, r));
                 }
-                let c = data.cacheDuration || Infinity;
-                if (data.timestamp > Date.now() - c) {
-                    return new Promise(r => r(data.data));
-                }
+                return new Promise(r => r(data.data));
             }
         }
         // if it's an api resource, refresh it
@@ -61,8 +77,6 @@ const cache = {
             //}
             return cache.update(id, cacheDuration, false);
         }
-        // if it's too old, remove it and return null
-        cache.remove(id);
         return new Promise(r => r(null));
     },
     update: (url, cacheDuration = 60*1000, persistant = false) => {
@@ -76,6 +90,11 @@ const cache = {
             delete(cache.events[url]);
             return new Promise(r => r(res));
         });
+    },
+    cleanCache: () => {
+        for (let key in window.localStorage) {
+            cache.handleCache(key);
+        }
     },
     resetCache: () => {
         cache.data = {};
