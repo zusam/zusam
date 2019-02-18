388ce36b586fcee99b61db184322dc41f9128e3b
Niels Robin-Aubertin <git@niels.fr>
2019-02-18 22:57:04 +0100

    Fix bandcamp embed

--- app/src/message/embeds/bandcamp-embed.component.js
+++ app/src/message/embeds/bandcamp-embed.component.js
@@ -4,7 +4,7 @@ export default class BandCampEmbed extends Component {
     render() {
         const embedUrl = this.props.url + "/size=large/bgcol=f5f5f5/linkcol=0687f5/minimal=true/tracklist=false/";
         return (
-            <div class="embed-responsive embed-responsive-1by1 max-width-450px-md">
+            <div class="bandcamp-embed embed-responsive embed-responsive-1by1">
                 <iframe class="embed-responsive-item" src={embedUrl}></iframe>
             </div>
         );
