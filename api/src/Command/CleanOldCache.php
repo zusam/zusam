a7c1ab42aa3ad8a76fc16b4c07f081a6225dff47
Niels Robin-Aubertin <git@niels.fr>
2019-07-07 11:17:58 +0200

    Rework cleanOldCache cmd to work on max cache size

--- api/src/Command/CleanOldCache.php
+++ api/src/Command/CleanOldCache.php
@@ -5,6 +5,7 @@ namespace App\Command;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -38,6 +39,7 @@ class CleanOldCache extends Command
     {
         $this->setName('zusam:clean-old-cache')
             ->setDescription('Clean old cache files')
+            ->addArgument('max-cache-size', InputArgument::OPTIONAL, 'Maximum cache usage in Mo (defaults to 512).')
             ->addOption('only-list', null, InputOption::VALUE_NONE, 'Only list files that would be deleted.')
             ->setHelp('This command removes cache files older than one month.');
     }
@@ -45,18 +47,36 @@ class CleanOldCache extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->logger->info("zusam:clean-old-cache");
+        $max_cache_size = 1024 * 1024 * ($input->getArgument('max-cache-size') ?? 512);
+
+        $cache_size = 0;
+        $files = [];
         foreach(scandir($this->targetDir."/images/") as $file) {
-            if (
-                $file != "." && $file != ".." &&
-                filemtime($this->targetDir."/images/".$file) < time() - 60*60*24*30
-            ) {
-                if ($input->getOption("verbose") || $input->getOption("only-list")) {
-                    echo $this->targetDir."/$file\n";
-                }
-                if (!$input->getOption("only-list")) {
-                    unlink($this->targetDir."/images/".$file);
-                }
+            if ($file != "." && $file != "..") {
+                $files[] = [
+                    "path" => $this->targetDir."/images/".$file,
+                    "mtime" => filemtime($this->targetDir."/images/".$file),
+                    "size" => filesize($this->targetDir."/images/".$file),
+                ];
+                $cache_size += filesize($this->targetDir."/images/".$file);
             }
+        }
+
+        usort($files, function ($f1, $f2) {
+            return $f1["mtime"] - $f2["mtime"];
+        });
+
+        foreach ($files as $file) {
+            if ($cache_size < $max_cache_size) {
+                break;
+            }
+            if ($input->getOption("verbose") || $input->getOption("only-list")) {
+                $output->writeln([$file["path"]]);
+            }
+            if (!$input->getOption("only-list")) {
+                unlink($file["path"]);
+            }
+            $cache_size -= $file["size"];
         }
     }
 }
