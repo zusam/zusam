489877a67292e6a078883e7407e608c04b528f17
Niels Robin-Aubertin <git@niels.fr>
2019-08-08 14:57:36 +0200

    Add option to seed init data

--- api/src/Command/Initialize.php
+++ api/src/Command/Initialize.php
@@ -5,6 +5,7 @@ namespace App\Command;
 use App\Entity\Group;
 use App\Entity\Message;
 use App\Entity\User;
+use App\Service\Uuid;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
@@ -33,11 +34,12 @@ class Initialize extends Command
     protected function configure()
     {
         $this->setName("zusam:init")
-            ->setDescription("Initialize the database with a first user and group.")
-            ->addArgument("user", InputArgument::REQUIRED, "What's the name of the first user ?")
-            ->addArgument("group", InputArgument::REQUIRED, "What's the name of the first group ?")
-            ->addArgument("password", InputArgument::REQUIRED, "What's the password of the first user ?")
-            ->addOption('remove-existing', null, InputOption::VALUE_NONE, 'Remove existing database beforehand.');
+             ->setDescription("Initialize the database with a first user and group.")
+             ->addArgument("user", InputArgument::REQUIRED, "What's the name of the first user ?")
+             ->addArgument("group", InputArgument::REQUIRED, "What's the name of the first group ?")
+             ->addArgument("password", InputArgument::REQUIRED, "What's the password of the first user ?")
+             ->addOption('remove-existing', null, InputOption::VALUE_NONE, 'Remove existing database beforehand.')
+             ->addOption('seed', null, InputOption::VALUE_REQUIRED, 'Specify a seed for UUIDs generation.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -65,9 +67,27 @@ class Initialize extends Command
         $user->setLogin($input->getArgument("user"));
         $user->setName(explode("@", $input->getArgument("user"))[0]);
         $user->setPassword($this->encoder->encodePassword($user, $input->getArgument("password")));
+        if ($input->getOption("seed")) {
+            $reflection = new \ReflectionClass($user);
+            $id = $reflection->getProperty("id");
+            $id->setAccessible(true);
+            $id->setValue($user, Uuid::uuidv4($input->getOption("seed")."_user"));
+            $secretKey = $reflection->getProperty("secretKey");
+            $secretKey->setAccessible(true);
+            $secretKey->setValue($user, Uuid::uuidv4($input->getOption("seed")."_user_secret_key"));
+        }
 
         $group = new Group();
         $group->setName($input->getArgument("group"));
+        if ($input->getOption("seed")) {
+            $reflection = new \ReflectionClass($group);
+            $id = $reflection->getProperty("id");
+            $id->setAccessible(true);
+            $id->setValue($group, Uuid::uuidv4($input->getOption("seed")."_group"));
+            $secretKey = $reflection->getProperty("secretKey");
+            $secretKey->setAccessible(true);
+            $secretKey->setValue($group, Uuid::uuidv4($input->getOption("seed")."_group_secret_key"));
+        }
 
         $group->addUser($user);
         $user->addGroup($group);
@@ -82,6 +102,15 @@ class Initialize extends Command
                 Try to post something yourself by using the new message button on the group page or by leaving a comment here.
             ",
         ]);
+        if ($input->getOption("seed")) {
+            $reflection = new \ReflectionClass($message_1);
+            $id = $reflection->getProperty("id");
+            $id->setAccessible(true);
+            $id->setValue($message_1, Uuid::uuidv4($input->getOption("seed")."_message"));
+            $secretKey = $reflection->getProperty("secretKey");
+            $secretKey->setAccessible(true);
+            $secretKey->setValue($message_1, Uuid::uuidv4($input->getOption("seed")."_message_secret_key"));
+        }
 
         $this->em->persist($user);
         $this->em->persist($group);
