ff245ae7fe0300630322db1b400655790e5336bb
Niels Robin-Aubertin <git@niels.fr>
2019-09-03 16:15:11 +0200

    Rewrite CreateTest

--- api/tests/Controller/Message/CreateTest.php
+++ api/tests/Controller/Message/CreateTest.php
@@ -6,7 +6,7 @@ use App\Entity\Message;
 use App\Tests\ApiTestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class NewTest extends ApiTestCase
+class CreateTest extends ApiTestCase
 {
     public function __construct() {
         parent::__construct();
@@ -14,10 +14,16 @@ class NewTest extends ApiTestCase
 
     public function testBadRequest()
     {
-        $this->client->request("POST", "/messages", [], [], ["CONTENT_TYPE" => "application/json"], "");
+        $this->client->request("POST", "/messages", [], [], [
+            "CONTENT_TYPE" => "application/json",
+            "HTTP_X_AUTH_TOKEN" => $this->firstUser->getSecretKey(),
+        ], "");
         $this->assertEquals(400, $this->client->getResponse()->getStatusCode(), "Empty body should be considered as bad content");
 
-        $this->client->request("POST", "/messages", [], [], ["CONTENT_TYPE" => "application/json"], "simple string");
+        $this->client->request("POST", "/messages", [], [], [
+            "CONTENT_TYPE" => "application/json",
+            "HTTP_X_AUTH_TOKEN" => $this->firstUser->getSecretKey(),
+        ], "simple string");
         $this->assertEquals(400, $this->client->getResponse()->getStatusCode(), "Not JSON body should be considered as bad content");
 
         //$client->request("POST", "/messages", [], [], [
@@ -43,27 +49,26 @@ class NewTest extends ApiTestCase
     {
         $this->client->request("POST", "/messages", [], [], [
             "CONTENT_TYPE" => "application/json",
-            "HTTP_X_AUTH_TOKEN" => "52ceecc0-4cdb-4500-897c-676c2ca632db",
+            "HTTP_X_AUTH_TOKEN" => $this->firstUser->getSecretKey(),
         ], '{
             "createdAt": '.time().',
             "lastActivityDate": '.time().',
-            "group": "/groups/f7140199-67e2-45c0-B596-a7a29c1fc901",
-            "author": "/users/11752f7e-9624-4986-89b8-636a081262ea",
+            "group": "'.$this->firstGroup->getId().'",
+            "author": "'.$this->firstUser->getId().'",
             "data": {"text": ""},
             "files": {},
             "children": {}
         }');
-        $this->assertEquals(201, $this->client->getResponse()->getStatusCode(), "Failed to post simple test message");
+        $this->assertEquals(201, $this->client->getResponse()->getStatusCode(), "Failed to post simple test message, response: " . $this->client->getResponse()->getContent());
 
         $content = json_decode($this->client->getResponse()->getContent(), true);
 
         $this->assertNotEmpty($content["id"], "Returned content should have an id");
-        $this->assertNotEmpty($content["secretKey"], "Returned content should have a secretKey");
+        $this->assertNotEmpty($content["createdAt"], "Returned content should have an id");
 
         $message = $this->em->getRepository(Message::class)->findOneById($content["id"]);
 
         $this->assertInstanceOf(Message::class, $message, "Entity should be a Message");
-        $this->assertEquals($content["secretKey"], $message->getSecretKey(), "SecretKey between content returned and persisted entity should be equal");
         $this->assertEquals($content["createdAt"], $message->getCreatedAt(), "CreatedAt between content returned and persisted entity should be equal");
     }
 }
