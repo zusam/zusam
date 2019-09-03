eeb3e3ced8b9322223f00a4bd1f46b580f61d428
Niels Robin-Aubertin <git@niels.fr>
2019-09-03 16:14:22 +0200

    Retab logger

--- api/src/Service/Logger.php
+++ api/src/Service/Logger.php
@@ -9,37 +9,37 @@ use Monolog\Logger as MonologLogger;
 
 class Logger extends AbstractProcessingHandler
 {
-	private $em;
+    private $em;
 
-	public function __construct(EntityManagerInterface $em, int $level = MonologLogger::DEBUG, bool $bubble = true) {
-		parent::__construct($level, $bubble);
-		$this->em = $em;
-	}
+    public function __construct(EntityManagerInterface $em, int $level = MonologLogger::DEBUG, bool $bubble = true) {
+        parent::__construct($level, $bubble);
+        $this->em = $em;
+    }
 
-	protected function write(array $record): void
-	{
-		$log = new Log();
+    protected function write(array $record): void
+    {
+        $log = new Log();
 
-		if (isset($record["message"])) {
-			$log->setMessage($record["message"]);
-		}
-		if (isset($record["channel"])) {
-			$log->setChannel($record["channel"]);
-		}
-		if (isset($record["level"])) {
-			$log->setLevel($record["level"]);
-		}
-		if (isset($record["levelName"])) {
-			$log->setLevelName($record["levelName"]);
-		}
-		if (isset($record["extra"])) {
-			$log->setExtra($record["extra"]);
-		}
-		if (isset($record["context"])) {
-			$log->setContext($record["context"]);
-		}
+        if (isset($record["message"])) {
+            $log->setMessage($record["message"]);
+        }
+        if (isset($record["channel"])) {
+            $log->setChannel($record["channel"]);
+        }
+        if (isset($record["level"])) {
+            $log->setLevel($record["level"]);
+        }
+        if (isset($record["levelName"])) {
+            $log->setLevelName($record["levelName"]);
+        }
+        if (isset($record["extra"])) {
+            $log->setExtra($record["extra"]);
+        }
+        if (isset($record["context"])) {
+            $log->setContext($record["context"]);
+        }
 
-		$this->em->persist($log);
-		$this->em->flush();
-	}
+        $this->em->persist($log);
+        $this->em->flush();
+    }
 }
