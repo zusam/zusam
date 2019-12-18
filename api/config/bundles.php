3d73e6cadae0b380372a3a7fe411c97c1922deb6
Niels Robin-Aubertin <git@niels.fr>
2019-12-18 13:28:03 +0100

    Rework monolog config: we don't want to log to the database anymore

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -11,6 +11,5 @@ return [
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
