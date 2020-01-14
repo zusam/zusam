fdf0a429d6cff804ef90ee66ef3643307c8175ae
Niels Robin-Aubertin <git@niels.fr>
2020-01-14 13:56:10 +0100

    Scroll to new comment from notification

--- app/src/components/notification.component.js
+++ app/src/components/notification.component.js
@@ -98,7 +98,7 @@ export default class Notification extends Component {
       case "new_message":
         return "/messages/" + this.props.target;
       case "new_comment":
-        return "/messages/" + this.props.fromMessage.id;
+        return "/messages/" + this.props.fromMessage.id + "/" + this.props.target;
       case "global_notification":
         return this.props.target;
       default:
