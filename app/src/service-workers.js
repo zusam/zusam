e97455419dcbdd7a3669f1166ca46d685c2dcd51
Niels Robin-Aubertin <git@niels.fr>
2020-03-29 22:36:52 +0200

    Generalize cache timeouts

--- app/src/service-workers.js
+++ app/src/service-workers.js
@@ -1,55 +1,75 @@
-const CACHE = 'zusam-4.1-simplecache-0.2';
+import { Store, set, get } from "idb-keyval";
+
+const ZUSAM_VERSION = "4.1";
+const CACHE_VERSION = "0.2";
+const CACHE = "zusam-" + ZUSAM_VERSION + "-simplecache-" + CACHE_VERSION;
+const cache_store = new Store("zusam-" + ZUSAM_VERSION, CACHE);
+const cached_routes = {
+  "/api/users/": 1000 * 60 * 60 * 24, // 24h
+  "/api/images/crop/": null,
+  "/api/images/thumbnail/": null,
+  "/api/links/by_url?": null
+};
 
 // On fetch, use cache but update the entry with the latest contents
 // from the server.
-self.addEventListener('fetch', function(evt) {
+self.addEventListener("fetch", function(evt) {
   if (evt.request.method == "GET") {
-    if (isCacheableUrl(evt.request.url)) {
+    // cache-update routes: retrieve from cache and update in background
+    if (Object.keys(cached_routes).some(r => evt.request.url.includes(r))) {
       // You can use `respondWith()` to answer immediately, without waiting for the
       // network response to reach the service worker...
       evt.respondWith(fromCache(evt.request));
-      // ...and `waitUntil()` to prevent the worker from being killed until the
-      // cache is updated.
-      evt.waitUntil(update(evt.request));
+
+      // ...and `waitUntil()` to prevent the worker from being killed until the function is finished.
+      evt.waitUntil(_ => {
+        get(evt.request.url, cache_store)
+          .then(r => {
+            if (r && r.hasOwnProperty("lastUsed") && r.lastUsed != null) {
+              const timeout =
+                r.lastUsed +
+                cached_routes[
+                  Object.keys(cached_routes).find(r =>
+                    evt.request.url.includes(r)
+                  )
+                ];
+              // update the cache only if the timeout is reached
+              if (timeout < Date.now()) {
+                update(evt.request);
+              }
+            }
+          })
+          .catch(_ => update(evt.request));
+      });
     } else {
+      // from-network without cache is the default
       return fromNetwork(evt.request, false);
     }
   }
 });
 
-// Return true if the url is one we want to cache
-function isCacheableUrl(url) {
-  const routes = [
-    '/api/images/crop/',
-    '/api/images/thumbnail/',
-    '/api/links/by_url?',
-    '/api/users/',
-  ];
-  return routes.some(r => url.includes(r));
-}
-
 // Make a network request, return the result and add it to cache if asked
 function fromNetwork(request, addToCache) {
-    return fetch(request).then(function (response) {
-      if (addToCache) {
-        // response may be used only once
-        // we need to save clone to put one copy in cache
-        // and serve second one
-        let responseClone = response.clone();
-        caches.open(CACHE).then(function (cache) {
-          cache.put(request, responseClone);
-        });
-      }
-      return response;
-    });
+  return fetch(request).then(function(response) {
+    if (addToCache) {
+      // response may be used only once
+      // we need to save clone to put one copy in cache
+      // and serve second one
+      let responseClone = response.clone();
+      caches.open(CACHE).then(function(cache) {
+        addToCache(request, responseClone);
+      });
+    }
+    return response;
+  });
 }
 
 // Open the cache where the assets were stored and search for the requested
 // resource. Notice that in case of no matching, the promise still resolves
 // but it does with `undefined` as value.
 function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
+  return caches.open(CACHE).then(function(cache) {
+    return cache.match(request).then(function(matching) {
       // if nothing matches, return response from network
       return matching || fromNetwork(request, true);
     });
@@ -59,9 +79,20 @@ function fromCache(request) {
 // Update consists in opening the cache, performing a network request and
 // storing the new response data.
 function update(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
+  return caches.open(CACHE).then(function(cache) {
+    return fetch(request).then(function(response) {
+      return addToCache(request, response);
     });
   });
+}
+
+// Add response to cache and store the lastUsed timestamp at the same time
+function addToCache(request, response) {
+  return set(
+    request.url,
+    {
+      lastUsed: Date.now()
+    },
+    cache_store
+  ).then(_ => cache.put(request, response));
 }
