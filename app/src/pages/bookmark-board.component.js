234ef220ab0436823d58864809428d0f60cd55e3
Niels Robin-Aubertin <git@niels.fr>
2020-08-02 21:07:03 +0200

    Don't return length if there's no bookmark

--- app/src/pages/bookmark-board.component.js
+++ app/src/pages/bookmark-board.component.js
@@ -25,32 +25,33 @@ export default class BookmarkBoard extends Component {
   }
 
   render() {
+    if (!Array.isArray(this.state.messages) || this.state.messages.length < 1) {
+      return;
+    }
     return (
-      Array.isArray(this.state.messages) && this.state.messages.length && (
-        <div>
-          <a
-            href={router.toApp("/bookmarks")}
-            onClick={e => router.onClick(e)}
-            class="no-decoration"
-          >
-            <div class="group-name">{lang.t("bookmarks")}</div>
-          </a>
-          <article id="group" class="justify-content-center d-flex">
-            <div class="message-container container-fluid d-flex justify-content-center flex-wrap">
-              {this.state.messages.map((msg, i) => {
-                return (
-                  <MessagePreview
-                    tabindex={i + 1}
-                    key={msg.id}
-                    message={msg}
-                    groupId={msg.group.id}
-                  />
-                );
-              })}
-            </div>
-          </article>
-        </div>
-      )
+      <div>
+        <a
+          href={router.toApp("/bookmarks")}
+          onClick={e => router.onClick(e)}
+          class="no-decoration"
+        >
+          <div class="group-name">{lang.t("bookmarks")}</div>
+        </a>
+        <article id="group" class="justify-content-center d-flex">
+          <div class="message-container container-fluid d-flex justify-content-center flex-wrap">
+            {this.state.messages.map((msg, i) => {
+              return (
+                <MessagePreview
+                  tabindex={i + 1}
+                  key={msg.id}
+                  message={msg}
+                  groupId={msg.group.id}
+                />
+              );
+            })}
+          </div>
+        </article>
+      </div>
     );
   }
 }
