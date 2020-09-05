1938b59c5dd3eef8c6f86f10e747526560c6e60a
Niels Robin-Aubertin <git@niels.fr>
2020-09-05 18:23:58 +0200

    Add /info route to api doc

--- api/src/Controller/Info.php
+++ api/src/Controller/Info.php
@@ -7,28 +7,35 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use Swagger\Annotations as SWG;
 
 class Info extends ApiController
 {
-  /**
-   * @Route("/info", methods={"GET", "HEAD"})
-   */
-  public function index(): Response
-  {
+    /**
+     * @Route("/info", methods={"GET"})
+     * @SWG\Response(
+     *  response=200,
+     *  description="Get informations about the API",
+     * )
+     */
+    public function index(): Response
+    {
 
     // check if ffmpeg is found
-    $has_ffmpeg = false;
-    if (is_executable(realpath($this->getParameter('binaries.ffmpeg')))) {
-      $has_ffmpeg = true;
-    }
+        $has_ffmpeg = false;
+        if (is_executable(realpath($this->getParameter('binaries.ffmpeg')))) {
+            $has_ffmpeg = true;
+        }
 
-    // check if imagick is enabled
-    $has_imagick = false;
-    if (extension_loaded('imagick')) {
-      $has_imagick = true;
-    }
+        // check if imagick is enabled
+        $has_imagick = false;
+        if (extension_loaded('imagick')) {
+            $has_imagick = true;
+        }
 
-    return new JsonResponse([
+        return new JsonResponse([
       'version' => $this->getParameter('version'),
       'upload' => [
         'image' => $this->getParameter('allow.upload.image') == 'true' && $has_imagick,
@@ -37,5 +44,5 @@ class Info extends ApiController
       'default_lang' => $this->getParameter('lang'),
       'allow_email' => $this->getParameter('allow.email'),
     ], JsonResponse::HTTP_OK);
-  }
+    }
 }
