e0daf91cf9781d05511057bad4e8910f68745363
Niels Robin-Aubertin <git@niels.fr>
2020-12-29 16:10:41 +0100

    Rework options of PreparePreviews command

--- api/src/Command/PreparePreviews.php
+++ api/src/Command/PreparePreviews.php
@@ -8,7 +8,7 @@ use App\Service\Url as UrlService;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -39,14 +39,16 @@ class PreparePreviews extends Command
     {
         $this->setName('zusam:prepare-previews')
             ->setDescription('Prepare-previews of parent messages.')
-            ->addArgument('memory', InputArgument::OPTIONAL, 'Maximum RAM usage (defaults to 70Mo).')
+            ->addOption('force', null, InputOption::VALUE_NONE, 'Reprepare preview already processed.')
+            ->addOption('memory', null, InputOption::VALUE_REQUIRED, 'Maximum RAM usage (defaults to 70Mo).', 70)
+            ->addOption('filter', null, InputOption::VALUE_REQUIRED, 'Filter previews to process (substring of url).', null)
             ->setHelp('This command preprocesses previews of parent messages for a faster first load.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->logger->info($this->getName());
-        $max_memory = $input->getArgument('memory') ? intval($input->getArgument('memory')) : 70;
+        $max_memory = $input->getOption('memory') ? intval($input->getOption('memory')) : 70;
         ini_set('memory_limit', max(128, $max_memory + 10).'M');
 
         // stats
@@ -56,7 +58,8 @@ class PreparePreviews extends Command
         $number_descriptions = 0;
         $number_titles = 0;
 
-        $c = $this->pdo->query('SELECT id, data FROM message WHERE parent_id IS NULL AND preview_id IS NULL ORDER BY created_at DESC;');
+
+        $c = $this->pdo->query('SELECT id, data FROM message ORDER BY created_at DESC;');
         $messages = [];
         while ($i = $c->fetch()) {
             $messages[] = $i;
@@ -77,10 +80,22 @@ class PreparePreviews extends Command
             // get first url data
             $text = json_decode($i['data'], true)['text'];
             $urls = Message::getUrlsFromText($text);
+
+            // apply filter if any
+            if ($input->getOption('filter')) {
+                $filter = $input->getOption('filter');
+                $urls = array_filter($urls, function($k) use ($filter) {
+                    return mb_stripos($k, $filter) !== false;
+                });
+            }
+
             if (count($urls) > 0) {
                 try {
+                    if ($output->isVerbose()) {
+                        $output->writeln([$urls[0]]);
+                    }
                     ++$number_links;
-                    $link = $this->urlService->getLink($urls[0]);
+                    $link = $this->urlService->getLink($urls[0], $input->getOption('force'));
                     $this->em->persist($link);
                 } catch (\Exception $e) {
                     $output->writeln([$e->getMessage()]);
