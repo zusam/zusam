a1ec3c2af412586806fa1565c11631876a548d10
Niels Robin-Aubertin <git@niels.fr>
2021-11-08 15:47:08 +0100

    Comment out image-service in client reduction for now

--- app/src/lazy/image-service.js
+++ app/src/lazy/image-service.js
@@ -1,39 +1,40 @@
-const reduceImport = require("image-blob-reduce");
-const reduce = reduceImport.default();
+//const reduceImport = require("image-blob-reduce");
+//const reduce = reduceImport.default();
 const imageService = {
   handleImage: (source, callback) => {
-    let fileSize = source.size;
-    try {
-      // don't use image reduction for iOS as it's problematic.
-      // TODO: Find a fix and test those platforms.
-      let iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-      if (iOS) {
-        console.warn("Do not use image reduction on iOS !");
-        callback(source);
-        return;
-      }
-      if (!source.type || !source.type.match(/image/)) {
-        console.warn("Do not use image reduction on invalid file !");
-        callback(source);
-        return;
-      }
-      if (source.type == "image/gif") {
-        console.warn("Do not use image reduction on a gif !");
-        callback(source);
-        return;
-      }
-      if (fileSize < 1024 * 256) {
-        console.warn("Do not use image reduction on small file !");
-        callback(source);
-        return;
-      }
-      reduce.toBlob(source, {max: 2048, unsharpAmount: 100, unsharpRadius: 1, quality: 2}).then(blob => callback(blob));
-    } catch (error) {
-      // TODO error logging
-      console.warn(error);
-      // If something goes wrong in image reduction, fall back to normal upload
-      callback(source);
-    }
+    callback(source);
+    //let fileSize = source.size;
+    //try {
+    //  // don't use image reduction for iOS as it's problematic.
+    //  // TODO: Find a fix and test those platforms.
+    //  let iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    //  if (iOS) {
+    //    console.warn("Do not use image reduction on iOS !");
+    //    callback(source);
+    //    return;
+    //  }
+    //  if (!source.type || !source.type.match(/image/)) {
+    //    console.warn("Do not use image reduction on invalid file !");
+    //    callback(source);
+    //    return;
+    //  }
+    //  if (source.type == "image/gif") {
+    //    console.warn("Do not use image reduction on a gif !");
+    //    callback(source);
+    //    return;
+    //  }
+    //  if (fileSize < 1024 * 256) {
+    //    console.warn("Do not use image reduction on small file !");
+    //    callback(source);
+    //    return;
+    //  }
+    //  reduce.toBlob(source, {max: 2048, unsharpAmount: 100, unsharpRadius: 1, quality: 2}).then(blob => callback(blob));
+    //} catch (error) {
+    //  // TODO error logging
+    //  console.warn(error);
+    //  // If something goes wrong in image reduction, fall back to normal upload
+    //  callback(source);
+    //}
   }
 };
 export default imageService;
