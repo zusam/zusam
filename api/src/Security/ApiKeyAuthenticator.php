b3939326d1a26c43dad9e4bed6c7e666d7184969
Niels Robin-Aubertin <git@niels.fr>
2023-09-21 17:01:17 -0400

    Modernize and simplify access control

--- api/src/Security/ApiKeyAuthenticator.php
+++ api/src/Security/ApiKeyAuthenticator.php
@@ -2,60 +2,30 @@
 
 namespace App\Security;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Http\AccessToken\AccessTokenHandlerInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 
-// https://symfony.com/index.php/doc/current/security/custom_authenticator.html
-class ApiKeyAuthenticator extends AbstractAuthenticator
+use App\Entity\User;
+
+// https://symfony.com/doc/current/security/access_token.html#using-the-access-token-authenticator
+class ApiKeyAuthenticator implements AccessTokenHandlerInterface
 {
-    /**
-     * Called on every request to decide if this authenticator should be
-     * used for the request. Returning false will cause this authenticator
-     * to be skipped.
-     */
-    public function supports(Request $request): ?bool
-    {
-        return $request->headers->has('X-AUTH-TOKEN');
+    private $em;
+
+    public function __construct(
+        EntityManagerInterface $em,
+    ) {
+        $this->em = $em;
     }
 
-    /**
-     * Called on every request. Return whatever credentials you want to
-     * be passed to getUser() as $credentials.
-     */
-    public function authenticate(Request $request): Passport
+    public function getUserBadgeFrom(string $apiKey): UserBadge
     {
-        $apiToken = $request->headers->get('X-AUTH-TOKEN');
-        if (null === $apiToken) {
-            // The token header was empty, authentication fails with HTTP Status
-            // Code 401 "Unauthorized"
-            throw new CustomUserMessageAuthenticationException('No API token provided');
+        $user = $this->em->getRepository(User::class)->findOneBySecretKey($apiKey);
+        if (null === $user->getSecretKey()) {
+            throw new BadCredentialsException('Invalid credentials.');
         }
 
-        return new SelfValidatingPassport(new UserBadge($apiToken));
-    }
-
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
-    {
-        // on success, let the request continue
-        return null;
-    }
-
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
-    {
-        $data = [
-          'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
-        ];
-
-        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+        return new UserBadge($user->getSecretKey());
     }
 }
