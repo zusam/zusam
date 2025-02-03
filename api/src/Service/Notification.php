6635633c7e5faec47feeabb16bd36de7c3fe777c
Neriderc <89974636+Neriderc@users.noreply.github.com>
2025-02-03 16:07:11 +1300

    Fix errors when user deleted (#94)

--- api/src/Service/Notification.php
+++ api/src/Service/Notification.php
@@ -73,11 +73,12 @@ class Notification
                 return '';
             }
             $author = $message->getAuthor();
+            $name = $author->getName();
         } catch (\Exception $e) {
             return '';
         }
 
-        return $author->getName();
+        return $name;
     }
 
     public function create($type, $target, $owner, $fromUser = null, $fromGroup = null, $fromMessage = null)
