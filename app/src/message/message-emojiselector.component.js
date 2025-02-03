092f7a4de3509ebc9c1c8e0e98631c44a4c7e2a8
Neriderc <89974636+Neriderc@users.noreply.github.com>
2025-02-03 22:13:07 +1300

    Lazy load reactions library
    Used lazy loading of reactions library so it is not downloaded by client when disabled.
    
    Library is imported when component is mounted so it works straight away for user when enabled.
    
    Bug in preact lazy loading required update to preact (see https://github.com/preactjs/preact/issues/2519). npm update has been run and updated package files committed.

--- app/src/message/message-emojiselector.component.js
+++ app/src/message/message-emojiselector.component.js
@@ -1,8 +1,9 @@
 import { h } from "preact";
 import { useState, useEffect, useRef } from "preact/hooks";
-import EmojiPicker from "emoji-picker-react";
 import { http } from "/src/core";
 import {useTranslation} from "react-i18next";
+import { lazy, Suspense } from "preact/compat";
+const EmojiPicker = lazy(() => import("emoji-picker-react"));
 
 export default function MessageEmojiSelector(props) {
   const [showPicker, setShowPicker] = useState(false);
@@ -24,6 +25,7 @@ export default function MessageEmojiSelector(props) {
   };
 
   useEffect(() => {
+    import("emoji-picker-react");
     if (showPicker) {
       document.addEventListener("mousedown", handleClickOutside);
     }
@@ -34,16 +36,18 @@ export default function MessageEmojiSelector(props) {
 
   return (
     <div ref={pickerRef} >
-      <div style={{cursor: "pointer"}} onClick={() => setShowPicker(!showPicker)}>➕ {t("react")}</div>
+      <div style={{cursor: "pointer"}} onClick={() => {if (EmojiPicker) setShowPicker(!showPicker);}}>➕ {t("react")}</div>
       {showPicker && (
         <div style={{position: "absolute", zIndex: 1000}}>
-          <EmojiPicker
-            onEmojiClick={handleEmojiClick}
-            emojiStyle="native"
-            reactionsDefaultOpen={true}
-            previewConfig={{defaultEmoji: "26aa", defaultCaption: "", showPreview: true}}
-            reactions={["1f44d", "2764-fe0f", "1f923", "1f622", "1f44e", "1f621"]}
-          />
+          <Suspense fallback={null}>
+            <EmojiPicker
+              onEmojiClick={handleEmojiClick}
+              emojiStyle="native"
+              reactionsDefaultOpen={true}
+              previewConfig={{defaultEmoji: "26aa", defaultCaption: "", showPreview: true}}
+              reactions={["1f44d", "2764-fe0f", "1f923", "1f622", "1f44e", "1f621"]}
+            />
+          </Suspense>
         </div>
       )}
     </div>
