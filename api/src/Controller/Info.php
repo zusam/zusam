a2fab4d467ead471d9b224ece76436a8e11149b9
Neriderc <89974636+Neriderc@users.noreply.github.com>
2025-06-20 18:16:57 +1200

    Limit /info if user logged out

--- api/src/Controller/Info.php
+++ api/src/Controller/Info.php
@@ -18,38 +18,42 @@ class Info extends ApiController
     #[Route('/info', methods: ['GET'])]
     public function index(): Response
     {
-        // check if ghostscript is found
-        $has_ghostscript = false;
-        if (is_executable(realpath($this->getParameter('binaries.ghostscript')))) {
-            $has_ghostscript = true;
-        }
+        $data = [
+            'default_lang' => $this->getParameter('lang'),
+            'allow_email' => 'true' == $this->getParameter('allow.email'),
+        ];
 
-        // check if ffmpeg is found
-        $has_ffmpeg = false;
-        if (is_executable(realpath($this->getParameter('binaries.ffmpeg')))) {
-            $has_ffmpeg = true;
-        }
+        if ($this->getUser()) {
+            // check if ghostscript is found
+            $has_ghostscript = false;
+            if (is_executable(realpath($this->getParameter('binaries.ghostscript')))) {
+                $has_ghostscript = true;
+            }
 
-        // check if imagick is enabled
-        $has_imagick = false;
-        if (extension_loaded('imagick')) {
-            $has_imagick = true;
-        }
+            // check if ffmpeg is found
+            $has_ffmpeg = false;
+            if (is_executable(realpath($this->getParameter('binaries.ffmpeg')))) {
+                $has_ffmpeg = true;
+            }
 
-        return new JsonResponse([
-          'version' => $this->getParameter('version'),
-          'upload' => [
-            'image' => 'true' == $this->getParameter('allow.upload.image') && $has_imagick,
-            'video' => 'true' == $this->getParameter('allow.upload.video') && $has_ffmpeg,
-            'pdf' => 'true' == $this->getParameter('allow.upload.pdf') && $has_ghostscript,
-          ],
-          'default_lang' => $this->getParameter('lang'),
-          'allow_email' => 'true' == $this->getParameter('allow.email'),
-          'allow_public_links' => 'true' == $this->getParameter('allow.public.links'),
-          'allow_message_reactions' => 'true' == $this->getParameter('allow.message.reactions'),
-          'show' => [
-            'group_invitation_links' => 'true' == $this->getParameter('show.group.invitation.links'),
-          ]
-        ], JsonResponse::HTTP_OK);
+            // check if imagick is enabled
+            $has_imagick = false;
+            if (extension_loaded('imagick')) {
+                $has_imagick = true;
+            }
+
+            $data['version'] = $this->getParameter('version');
+            $data['upload'] = [
+                'image' => 'true' == $this->getParameter('allow.upload.image') && $has_imagick,
+                'video' => 'true' == $this->getParameter('allow.upload.video') && $has_ffmpeg,
+                'pdf' => 'true' == $this->getParameter('allow.upload.pdf') && $has_ghostscript,
+            ];
+            $data['allow_public_links'] = 'true' == $this->getParameter('allow.public.links');
+            $data['allow_message_reactions'] = 'true' == $this->getParameter('allow.message.reactions');
+            $data['show'] = [
+                'group_invitation_links' => 'true' == $this->getParameter('show.group.invitation.links'),
+            ];
+        }
+        return new JsonResponse($data, JsonResponse::HTTP_OK);
     }
 }
