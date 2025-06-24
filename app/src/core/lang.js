eaa9db380d0545a7ed51bbf31c01a391b70737e3
Niels Robin-Aubertin <git@niels.fr>
2025-06-24 19:58:00 -0400

    Add new translated languages

--- app/src/core/lang.js
+++ app/src/core/lang.js
@@ -7,11 +7,14 @@ import cs_CZ from "/../translations/app/cs_CZ.json";
 import de_DE from "/../translations/app/de_DE.json";
 import en_US from "/../translations/app/en_US.json";
 import es_ES from "/../translations/app/es_ES.json";
+import fi_FI from "/../translations/app/fi_FI.json";
 import fr_FR from "/../translations/app/fr_FR.json";
 import hu_HU from "/../translations/app/hu_HU.json";
+import it_IT from "/../translations/app/it_IT.json";
 import ko_KR from "/../translations/app/ko_KR.json";
 import nb_NO from "/../translations/app/nb_NO.json";
 import nl_NL from "/../translations/app/nl_NL.json";
+import pl_PL from "/../translations/app/pl_PL.json";
 import pt_BR from "/../translations/app/pt_BR.json";
 import ru_RU from "/../translations/app/ru_RU.json";
 import sk_SK from "/../translations/app/sk_SK.json";
@@ -25,11 +28,14 @@ const lang = {
     de_DE: "Deutsch",
     en_US: "English",
     es_ES: "español",
+    fi_FI: "suomalainen",
     fr_FR: "français",
     hu_HU: "magyar nyelv",
+    it_IT: "italiano",
     ko_KR: "한국어",
     nb_NO: "Norge",
     nl_NL: "Nederlands",
+    pl_PL: "polski",
     pt_BR: "Português",
     ru_RU: "Русский",
     sk_SK: "Slovenský",
@@ -48,11 +54,14 @@ i18n.use(initReactI18next).init({
     de_DE: { translation: de_DE },
     en_US: { translation: en_US },
     es_ES: { translation: es_ES },
+    fi_FI: { translation: fi_FI },
     fr_FR: { translation: fr_FR },
     hu_HU: { translation: hu_HU },
+    it_IT: { translation: it_IT },
     ko_KR: { translation: ko_KR },
     nb_NO: { translation: nb_NO },
     nl_NL: { translation: nl_NL },
+    pl_PL: { translation: pl_PL },
     pt_BR: { translation: pt_BR },
     ru_RU: { translation: ru_RU },
     sk_SK: { translation: sk_SK },
